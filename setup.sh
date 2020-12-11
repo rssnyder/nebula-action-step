1deca9830fe720c09cac9a4c45c1281116f92fc6
Riley Snyder <rileysndr@gmail.com>
2020-12-11 16:43:02 -0600

    inital work

---
+++ setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Usage: ./setup.sh <interface name>
+#                   <private key>
+#                   <hub hostname>
+#                   <public key>
+#                   <interface ip>
+#                   <allowed ips>
+
+INTERFACE_NAME=$1
+PRIVATE_KEY=$2
+HUB_HOSTNAME=$3
+PUBLIC_KEY=$4
+INTERFACE_IP=$5
+ALLOWED_IPS=$6
+
+# Install wireguard
+
+sudo apt-get install wireguard
+
+# Configure wireguard
+
+cat >/etc/wireguard/$INTERFACE_NAME.conf << EOF
+[Interface]
+PrivateKey=$PRIVATE_KEY
+Address=$INTERFACE_IP/32
+
+[Peer]
+PublicKey=$PUBLIC_KEY
+Endpoint=$HUB_HOSTNAME
+AllowedIPs=$ALLOWED_IPS
+PersistentKeepalive=15
+EOF
+
+# Start wireguard
+
+sudo wg-quick up $INTERFACE_NAME
+
+wg show $INTERFACE_NAME
